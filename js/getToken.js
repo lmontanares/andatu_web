55320d997d8cafa663f693ab4ed98e559a5eb1a8
maldit0 <luciano.montanares@gmail.com>
2021-07-06 18:26:58 -0400

    testing js

---
+++ js/getToken.js
@@ -0,0 +1,39 @@
+﻿let formLogin = document.getElementById('form-login');
+
+formLogin.addEventListener('submit', function (event) {
+    // console.log("Form has been submited");
+    // console.log(event.target)
+
+    let data_ = {
+        'email': document.getElementById('email').value,
+        'password': document.getElementById('password').value
+    }
+
+    event.preventDefault()
+
+    let response_status
+    fetch(' http://127.0.0.1:8000/auth/token/login/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data_),
+    })
+        .then(response => {
+            response_status = response.status;
+            return response.json();
+        })
+        .then(data => {
+            if (response_status === 200) {
+
+                console.log('Success:', data.auth_token);
+                sessionStorage.setItem('Token', data.auth_token);
+                window.location.href = "home.html";
+            } else {
+                console.log('revise sus credenciales')
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
+});
---
+++ js/validacion.js
@@ -0,0 +1,65 @@
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const rut = document.getElementById("rut")
+const telefono = document.getElementById("telefono")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+const formlogin = document.getElementById("form-login")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+    if(rut.value.length < 8){
+        warnings += `El rut no es valido <br>`
+        entrar = true
+    }
+    if(telefono.value.length < 8){
+        warnings += `El telefono no es valido <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
+
+formlogin.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+   
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
